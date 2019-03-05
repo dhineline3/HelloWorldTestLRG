8f59c42d5b03c8cbf6910c9beea4247a60e18420
Daniel Hineline <danielhineline3@gmail.com>
2019-03-04 21:26:10 -0500

    Final

--- HelloWorldLargeTest/Program.cs
+++ HelloWorldLargeTest/Program.cs
@@ -10,6 +10,18 @@ namespace HelloWorldLargeTest
     {
         static void Main(string[] args)
         {
+            string userName = "";
+            int userAge = 0;
+            int currentYear = 0;
+
+            Console.WriteLine("Please enter your name:");
+            userName = Console.ReadLine();
+            Console.WriteLine("Please enter your age:" );
+            userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the current year: ");
+            currentYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello World! My name is {0} and I am {1} years old. I was born in {2}.", userName, userAge, currentYear - userAge);
         }
     }
 }
